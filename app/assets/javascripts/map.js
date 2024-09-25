e46d1cfb3cd20d42028270eb719130d1ca97506f
Ed Davey <ed@dxw.com>
2024-09-25 18:28:13 +0100

    Use WMS services to compose map with layers
    We obtain map tiles from two Web Map Services:
    
    - OpenStreetMap: OSM Overlay WMS - by terrestris
      http://ows.mundialis.de/services/service
      Layer name: OSM-Overlay-WMS
    
    - GeoServer Web Map Service for CERC
      https://airtext.info/geoserver/wms
      Layer name: london:Total
    
    See the article [WMS in Leaflet][] for details.
    
    [WMS in Leaflet]:
    https://leafletjs.com/examples/wms/wms.html

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,15 +1,32 @@
 document.addEventListener("turbolinks:load", function () {
-  console.log("Turbolinks loaded map.js file");
   const mapEle = document.querySelector("#map");
 
   if (mapEle) {
-    console.log("map placeholder element found. Will draw map...");
-    const map = L.map("map").setView([51.505, -0.09], 13);
+    const bigBenLatLng = [51.510357, -0.116773];
+    const map = L.map("map").setView(bigBenLatLng, 10);
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
+    const cercOptions = {
+      layers: "london:Total",
+      time: "2024-09-25",
+      format: "image/png",
+      transparency: true, // term used by CERC
+      styles: "daqiTotal",
+      // styles: "daqiTotal_linear"
+      //   we heard: "This can provide a more detailed map, at the expense of
+      //   less clearly defined bands."
+    };
+    const osmOptions = {
+      layers: "OSM-Overlay-WMS",
+      format: "image/png",
+      transparent: true, // standard term (?) used by Leaflet and OSM
+    };
+
+    // compose the map using our WMS layers
+    L.tileLayer
+      .wms("https://airtext.info/geoserver/wms?", cercOptions)
+      .addTo(map);
+    L.tileLayer
+      .wms("https://ows.mundialis.de/services/service?", osmOptions)
+      .addTo(map);
   }
 });
