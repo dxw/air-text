0c7aa131e9e793e81b72b6f5f6f1934731e0bc64
Patrick Fleming <patrick.fleming@dxw.com>
2024-10-01 16:28:36 +0100

    wip
    wip

--- spec/controllers/forecasts_controller_spec.rb
+++ spec/controllers/forecasts_controller_spec.rb
@@ -1,9 +1,12 @@
 RSpec.describe ForecastsController do
-  describe "GET :index" do
-    it "renders the _index_ template" do
-      get :index
+  describe "GET :show" do
+    it "renders the _show_ template" do
+      allow(CercApiClient).to receive(:fetch_data)
 
-      expect(response).to render_template("index")
+      get :show
+
+      expect(CercApiClient).to have_received(:fetch_data).with("Southwark").and_return
+      expect(response).to render_template("show")
     end
   end
 end
