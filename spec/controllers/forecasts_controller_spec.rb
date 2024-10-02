dd53af8a8165700d4bd480aa7d0e240ac360b366
Ed Davey <ed@dxw.com>
2024-10-02 14:22:10 +0100

    Fix up controller spec
    - stub environment variables
    - use an inline JSON fixture for clarity

--- spec/controllers/forecasts_controller_spec.rb
+++ spec/controllers/forecasts_controller_spec.rb
@@ -1,7 +1,51 @@
 RSpec.describe ForecastsController do
+  around do |example|
+    env_vars = {
+      CERC_API_HOST_URL: "https://cerc.example.com",
+      CERC_API_KEY: "SECRET-API-KEY"
+    }
+    ClimateControl.modify(env_vars) { example.run }
+  end
+
   describe "GET :show" do
+    let(:json) do
+      <<~JSON
+        {
+          "forecastdate": "01-10-2024 14:40",
+          "timestamp": 1727793654317.342,
+          "zones": [
+            {
+              "forecasts": [
+                {
+                  "NO2": 1,
+                  "O3": 2,
+                  "PM10": 1,
+                  "PM2.5": 1,
+                  "forecast_date": "2024-10-01",
+                  "non_pollution_version": null,
+                  "pollen": -999,
+                  "pollution_version": 202410011407,
+                  "rain_am": 1.31,
+                  "rain_pm": 3.01,
+                  "temp_max": 14.0,
+                  "temp_min": 10.4,
+                  "total": 2,
+                  "total_status": "LOW",
+                  "uv": 1,
+                  "wind_am": 5.3,
+                  "wind_pm": 6.0
+                }
+              ],
+              "zone_id": 14,
+              "zone_name": "Haringey",
+              "zone_type": 1
+            }
+          ]
+        }
+      JSON
+    end
     it "renders the _show_ template" do
-      allow(CercApiClient).to receive(:fetch_data).and_return({body: File.read("#{Rails.root}/public/sample-forecast.json")})
+      allow(CercApiClient).to receive(:fetch_data).and_return(JSON.parse(json))
 
       get :show
 
