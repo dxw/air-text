b26164a015272860bcd7d4386426fcc8e062670b
Patrick Fleming <patrick.fleming@dxw.com>
2024-10-04 11:59:51 +0100

    Move from forecasts#index -> forecasts#show method
    We have agree that this method is rendering a single, three day forecast at a time, therefore it should be a show rather than an index method, and forecast should be singular in the path.

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,5 +1,5 @@
 class ForecastsController < ApplicationController
-  def index
+  def show
     @forecast = JSON.parse(File.read("#{Rails.root}/public/sample-forecast.json"))["zones"][0]
     @dates = @forecast["forecasts"].map { |forecast| forecast["forecast_date"] }
   end
--- spec/controllers/forecasts_controller_spec.rb
+++ spec/controllers/forecasts_controller_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe ForecastsController do
-  describe "GET :index" do
-    it "renders the _index_ template" do
-      get :index
+  describe "GET :show" do
+    it "renders the _show_ template" do
 
       expect(response).to render_template("index")
+      get :show
     end
   end
 end
