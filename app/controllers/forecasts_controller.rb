49380f5553850935728d6ea2c769df4f6eae44cd
Patrick Fleming <patrick.fleming@dxw.com>
2024-10-04 11:56:55 +0100

    Update forecast page with live data and zone form
    Users can now select a zone and fetch live data for that zone from the CERC API

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,6 +1,10 @@
 class ForecastsController < ApplicationController
   def show
-    @forecast = JSON.parse(File.read("#{Rails.root}/public/sample-forecast.json"))["zones"][0]
-    @dates = @forecast["forecasts"].map { |forecast| forecast["forecast_date"] }
+    forecast_response = CercApiClient.fetch_data(params.has_key?("zone") ? params["zone"] : "Southwark")
+
+    @forecast = forecast_response["zones"].first
+
+    @dates = @forecast["forecasts"].map { |day_forecast| day_forecast["forecast_date"] }
+    @zones = JSON.parse(File.read("#{Rails.root}/config/list-of-zones.json"))
   end
 end
--- spec/controllers/forecasts_controller_spec.rb
+++ spec/controllers/forecasts_controller_spec.rb
@@ -1,9 +1,12 @@
 RSpec.describe ForecastsController do
   describe "GET :show" do
     it "renders the _show_ template" do
+      allow(CercApiClient).to receive(:fetch_data).and_return({body: File.read("#{Rails.root}/public/sample-forecast.json")})
 
-      expect(response).to render_template("index")
       get :show
+
+      expect(CercApiClient).to have_received(:fetch_data).with("Southwark")
+      expect(response).to render_template("show")
     end
   end
 end
