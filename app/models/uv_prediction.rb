e4c7af993b92e9ccb40de1e51c5dc59e48ba73f5
Ed Davey <ed@dxw.com>
2024-10-07 18:02:30 +0100

    Refactor UvPrediction to use DaqiProperties

--- app/models/uv_prediction.rb
+++ app/models/uv_prediction.rb
@@ -1,32 +1,10 @@
 class UvPrediction
+  include DaqiProperties
+
   attr_reader :value
 
   def initialize(value)
     @value = value
-  end
-
-  DAQI_LABELS = {
-    low: "Low",
-    moderate: "Moderate",
-    high: "High",
-    very_high: "Very high"
-  }
-
-  def daqi_label
-    DAQI_LABELS.fetch(daqi_level)
-  end
-
-  def daqi_level
-    case @value
-    when 1..3
-      :low
-    when 4..6
-      :moderate
-    when 7..9
-      :high
-    else
-      :very_high
-    end
   end
 
   def guidance
