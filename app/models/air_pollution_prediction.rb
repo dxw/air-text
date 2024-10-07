5b146a55d671489d4c566de5469d9e0d63310d72
Ed Davey <ed@dxw.com>
2024-10-07 18:03:34 +0100

    Refactor AirPollutionPrediction to use DaqiProperties

--- app/models/air_pollution_prediction.rb
+++ app/models/air_pollution_prediction.rb
@@ -1,4 +1,6 @@
 class AirPollutionPrediction
+  include DaqiProperties
+
   attr_reader :forecasted_at,
     :nitrogen_dioxide,
     :particulate_matter_10,
@@ -23,30 +25,6 @@ class AirPollutionPrediction
     @ozone = ozone
     @value = value
     @label = label
-  end
-
-  DAQI_LABELS = {
-    low: "Low",
-    moderate: "Moderate",
-    high: "High",
-    very_high: "Very high"
-  }
-
-  def daqi_label
-    DAQI_LABELS.fetch(daqi_level)
-  end
-
-  def daqi_level
-    case @value
-    when 1..3
-      :low
-    when 4..6
-      :moderate
-    when 7..9
-      :high
-    else
-      :very_high
-    end
   end
 
   # :nocov:
