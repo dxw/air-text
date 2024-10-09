3f38e5c01300ad304ad676c95d672df307b910c9
Ed Davey <ed@dxw.com>
2024-10-09 15:20:30 +0100

    Rename existing stylesheets as 'gov_uk_application.css'
    We'll remove this styling once we've transferred out
    existing functionality to the new styling.
    
    i.e. once our feature specs for the `/forecast` are passing
    with the new styling, currently at `/styled_forecast`.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,8 @@
 //= link_tree ../images
+
+// include our initial GOV.UK based stylesheets for now whilst
+//  migrating to ESBuild bundled assets...
+//= link_directory ../stylesheets .css
+
 //= link_directory ../javascripts .js
 //= link_tree ../builds
