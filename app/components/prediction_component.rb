7d6385234170327590c1c9eb825eb632e7b3b0c0
Ed Davey <ed@dxw.com>
2024-10-10 17:34:42 +0100

    Include DAQI level as a coloured indicator

--- app/components/prediction_component.rb
+++ app/components/prediction_component.rb
@@ -12,4 +12,19 @@ class PredictionComponent < ViewComponent::Base
   def daqi_level_for_class
     @prediction.daqi_level.to_s.parameterize
   end
+
+  def daqi_indicator_colour
+    case @prediction.daqi_level
+    when :low
+      "text-green-600"
+    when :moderate
+      "text-amber-300"
+    when :high
+      "text-red-500"
+    when :very_high
+      "text-stone-700"
+    else
+      raise "DAQI level '#{@prediction.daqi_level}' not known"
+    end
+  end
 end
--- spec/components/prediction_component_spec.rb
+++ spec/components/prediction_component_spec.rb
@@ -22,6 +22,67 @@ RSpec.describe PredictionComponent, type: :component do
     end
   end
 
+  describe "#daqi_indicator_colour" do
+    context "when the daqi level is low" do
+      let(:component) {
+        PredictionComponent.new(
+          prediction: OpenStruct.new(daqi_level: :low)
+        )
+      }
+
+      it "uses a green colour" do
+        expect(component.daqi_indicator_colour).to match(/green/)
+      end
+    end
+
+    context "when the daqi level is moderate" do
+      let(:component) {
+        PredictionComponent.new(
+          prediction: OpenStruct.new(daqi_level: :moderate)
+        )
+      }
+      it "uses an amber colour" do
+        expect(component.daqi_indicator_colour).to match(/amber/)
+      end
+    end
+
+    context "when the daqi level is high" do
+      let(:component) {
+        PredictionComponent.new(
+          prediction: OpenStruct.new(daqi_level: :high)
+        )
+      }
+
+      it "uses a red colour" do
+        expect(component.daqi_indicator_colour).to match(/red/)
+      end
+    end
+
+    context "when the daqi level is very high" do
+      let(:component) {
+        PredictionComponent.new(
+          prediction: OpenStruct.new(daqi_level: :very_high)
+        )
+      }
+
+      it "uses a black-ish" do
+        expect(component.daqi_indicator_colour).to match(/stone/)
+      end
+    end
+
+    context "when the daqi level is not known" do
+      let(:component) {
+        PredictionComponent.new(
+          prediction: OpenStruct.new(daqi_level: :unknown)
+        )
+      }
+
+      it "raises an error" do
+        expect { component.daqi_indicator_colour }.to raise_error(/DAQI level 'unknown' not known/)
+      end
+    end
+  end
+
   describe "daqi-level" do
     it "adds the DAQI level as a class on the wrapper" do
       expect(page).to have_css(".prediction.moderate")
@@ -29,6 +90,11 @@ RSpec.describe PredictionComponent, type: :component do
 
     it "includes an indicator with the level as a class" do
       expect(page).to have_css(".daqi-indicator.moderate", text: "●")
+    end
+
+    it "includes the daqi_indicator_colour as a class" do
+      component = PredictionComponent.new(prediction: prediction)
+      expect(page).to have_css(".#{component.daqi_indicator_colour}.daqi-indicator", text: "●")
     end
   end
 end
