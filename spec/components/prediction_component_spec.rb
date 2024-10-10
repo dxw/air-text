2969d2bf9ba786108c1ef6feb9d1f8c29d45617f
Ed Davey <ed@dxw.com>
2024-10-10 16:04:02 +0100

    Add name of prediction to wrapper as a class

--- spec/components/prediction_component_spec.rb
+++ spec/components/prediction_component_spec.rb
@@ -4,11 +4,16 @@ require "rails_helper"
 
 RSpec.describe PredictionComponent, type: :component do
   let(:prediction) {
-    OpenStruct.new(name: "Solar Rays")
+    OpenStruct.new(
+      name: "Solar Rays"
+    )
   }
 
   before { render_inline(PredictionComponent.new(prediction: prediction)) }
 
+  it "adds the name of the prediction as a class to the wrapper" do
+    expect(page).to have_css(".prediction.solar-rays")
+  end
   it "renders the #name in .name" do
     expect(page).to have_css(".name", text: "Solar Rays")
   end
