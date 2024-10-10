ea28b6952321cfc5008413ea9a5a0c049a6e8c03
Ed Davey <ed@dxw.com>
2024-10-10 16:26:59 +0100

    Refactor to move parameterisation to component

--- app/components/prediction_component.rb
+++ app/components/prediction_component.rb
@@ -4,4 +4,12 @@ class PredictionComponent < ViewComponent::Base
   def initialize(prediction:)
     @prediction = prediction
   end
+
+  def name_for_class
+    @prediction.name.parameterize
+  end
+
+  def daqi_level_for_class
+    @prediction.daqi_level.to_s.parameterize
+  end
 end
