d50c2b131bc41a3711db40a740b37471d6f7d16c
Ed Davey <ed@dxw.com>
2024-10-10 16:17:43 +0100

    Add #daqi_level as .daqi-indicator

--- spec/components/prediction_component_spec.rb
+++ spec/components/prediction_component_spec.rb
@@ -5,16 +5,30 @@ require "rails_helper"
 RSpec.describe PredictionComponent, type: :component do
   let(:prediction) {
     OpenStruct.new(
-      name: "Solar Rays"
+      name: "Solar Rays",
+      daqi_level: :moderate
     )
   }
 
   before { render_inline(PredictionComponent.new(prediction: prediction)) }
 
-  it "adds the name of the prediction as a class to the wrapper" do
-    expect(page).to have_css(".prediction.solar-rays")
+  describe "#name" do
+    it "adds the name of the prediction as a class to the wrapper" do
+      expect(page).to have_css(".prediction.solar-rays")
+    end
+
+    it "renders the #name in .name" do
+      expect(page).to have_css(".name", text: "Solar Rays")
+    end
   end
-  it "renders the #name in .name" do
-    expect(page).to have_css(".name", text: "Solar Rays")
+
+  describe "daqi-level" do
+    it "adds the DAQI level as a class on the wrapper" do
+      expect(page).to have_css(".prediction.moderate")
+    end
+
+    it "includes an indicator with the level as a class" do
+      expect(page).to have_css(".daqi-indicator.moderate", text: "●")
+    end
   end
 end
