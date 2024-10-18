a8629200bf34806a3cf19a5ddcc657d55220cdcf
Patrick Fleming <patrick.fleming@dxw.com>
2024-10-18 08:52:54 +0100

    controller test - wip

--- spec/controllers/styled_forecasts_controller_spec.rb
+++ spec/controllers/styled_forecasts_controller_spec.rb
@@ -7,46 +7,84 @@ RSpec.describe StyledForecastsController do
     ClimateControl.modify(env_vars) { example.run }
   end
 
+  let(:forecast_from_api) do
+    {
+      "forecastdate" => "01-10-2024 14:40",
+      "timestamp" => 1727793654317.342,
+      "zones" => [
+        {
+          "forecasts" => [
+            {
+              "NO2" => 1,
+              "O3" => 2,
+              "PM10" => 1,
+              "PM2.5" => 1,
+              "forecast_date" => "2024-10-01",
+              "non_pollution_version" => nil,
+              "pollen" => -999,
+              "pollution_version" => 202410011407,
+              "rain_am" => 1.31,
+              "rain_pm" => 3.01,
+              "temp_max" => 14.0,
+              "temp_min" => 10.4,
+              "total" => 2,
+              "total_status" => "LOW",
+              "uv" => 1,
+              "wind_am" => 5.3,
+              "wind_pm" => 6.0
+            },
+            {
+              "NO2" => 1,
+              "O3" => 2,
+              "PM10" => 1,
+              "PM2.5" => 1,
+              "forecast_date" => "2024-10-02",
+              "non_pollution_version" => nil,
+              "pollen" => -999,
+              "pollution_version" => 202410011407,
+              "rain_am" => 1.31,
+              "rain_pm" => 3.01,
+              "temp_max" => 14.0,
+              "temp_min" => 10.4,
+              "total" => 2,
+              "total_status" => "LOW",
+              "uv" => 1,
+              "wind_am" => 5.3,
+              "wind_pm" => 6.0
+            },
+            {
+              "NO2" => 1,
+              "O3" => 2,
+              "PM10" => 1,
+              "PM2.5" => 1,
+              "forecast_date" => "2024-10-03",
+              "non_pollution_version" => nil,
+              "pollen" => -999,
+              "pollution_version" => 202410011407,
+              "rain_am" => 1.31,
+              "rain_pm" => 3.01,
+              "temp_max" => 14.0,
+              "temp_min" => 10.4,
+              "total" => 2,
+              "total_status" => "LOW",
+              "uv" => 1,
+              "wind_am" => 5.3,
+              "wind_pm" => 6.0
+            }
+          ],
+          "zone_id" => 14,
+          "zone_name" => "Haringey",
+          "zone_type" => 1
+        }
+      ]
+    }
+  end
+
+  let(:forecasts) do
+    ForecastFactory.build(forecast_from_api)
+  end
+
   describe "GET :show" do
-    let(:forecast_from_api) do
-      {
-        "forecastdate" => "01-10-2024 14:40",
-        "timestamp" => 1727793654317.342,
-        "zones" => [
-          {
-            "forecasts" => [
-              {
-                "NO2" => 1,
-                "O3" => 2,
-                "PM10" => 1,
-                "PM2.5" => 1,
-                "forecast_date" => "2024-10-01",
-                "non_pollution_version" => nil,
-                "pollen" => -999,
-                "pollution_version" => 202410011407,
-                "rain_am" => 1.31,
-                "rain_pm" => 3.01,
-                "temp_max" => 14.0,
-                "temp_min" => 10.4,
-                "total" => 2,
-                "total_status" => "LOW",
-                "uv" => 1,
-                "wind_am" => 5.3,
-                "wind_pm" => 6.0
-              }
-            ],
-            "zone_id" => 14,
-            "zone_name" => "Haringey",
-            "zone_type" => 1
-          }
-        ]
-      }
-    end
-
-    let(:forecasts) do
-      ForecastFactory.build(forecast_from_api)
-    end
-
     it "obtains forecasts for the default zone (Southwark) from the CercApiClient" do
       allow(CercApiClient).to receive(:forecasts_for).and_return(forecasts)
 
@@ -62,6 +100,22 @@ RSpec.describe StyledForecastsController do
       get :show
 
       expect(response).to render_template("show")
+    end
+  end
+
+  describe "GET :update" do
+    let(:forecasts) do
+      ForecastFactory.build(forecast_from_api)
+    end
+
+    it "returns a predictions partial template within a turbo stream replace element" do
+      allow(CercApiClient).to receive(:forecasts_for).and_return(forecasts)
+      allow(Turbo::Streams::TagBuilder).to receive(:replace)
+
+      get :update, params: {day: :tomorrow}
+
+      expect(CercApiClient).to have_received(:forecasts_for).with("Southwark")
+      expect(Turbo::Streams::TagBuilder).to have_received(:replace).with("day_predictions", partial: "predictions", locals: {forecast: forecasts.second})
     end
   end
 end
