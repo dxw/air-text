7a089dff22104d1f5eb9fab31262fcc66031f4bb
Patrick Fleming <patrick.fleming@dxw.com>
2024-10-18 10:36:36 +0100

    Add JS handler for hamburger menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const menuList = document.getElementById("menu-list");
+
+  hamburgerMenu.addEventListener("click", function () {
+    menuList.classList.toggle("hidden");
+  });
+});
