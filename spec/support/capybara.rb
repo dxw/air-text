91bd2ec265e3f88ffe7350f28030efc137bc6602
Ed Davey <ed@dxw.com>
2024-10-21 17:34:44 +0100

    Add Cuprite gem for testing JS in Capybara
    From the docs at https://github.com/rubycdp/cuprite
    
    > Cuprite - Headless Chrome driver for Capybara
    >
    > Cuprite is a pure Ruby driver (read as no Selenium/WebDriver
    > /ChromeDriver dependency) for Capybara. It allows you to
    > run Capybara tests on a headless Chrome or Chromium.
    
    Capybara will use this driver if we pass in the `js: true`
    option to the scenario.
    
    If you run a spec with the INSPECTOR option e.g.
    
    ```
    rspec INSPECTOR=true bundle exec ./spec/features/visitors/view_styled_forecasts_spec.rb:33
    ```
    
    you can then enter a debugging session with:
    
    ```
    page.driver.debug(binding)
    ```
    
    and have access to the JS enable page.
    
    I think we'll need to add Chromium to our docker build for
    CI...
    
    Also see https://janko.io/upgrading-from-selenium-to-cuprite/
    for some further info on config. Janko is the author of the
    excellent Shrine library (https://github.com/shrinerb/shrine)
    for uploading files with Ruby.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,4 +2,18 @@
 
 Dir[Rails.root.join("spec", "feature_steps", "**", "*.rb")].sort.each { |f| require f }
 
+require "capybara/cuprite"
+Capybara.default_max_wait_time = 5
+Capybara.disable_animation = true
+Capybara.javascript_driver = :cuprite
+Capybara.register_driver(:cuprite) do |app|
+  Capybara::Cuprite::Driver.new(
+    app,
+    window_size: [1200, 800],
+    js_errors: true,
+    timeout: 10,
+    process_timeout: 15,
+    inspector: ENV["INSPECTOR"]
+  )
+end
 Capybara.asset_host = "http://localhost:3000"
