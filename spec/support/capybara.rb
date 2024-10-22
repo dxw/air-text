18cea53e5b2d00ca1064e9311003d61281b3c8d2
Ed Davey <ed@dxw.com>
2024-10-22 08:48:01 +0100

    Configure CI for JS tests with Cuprite/Ferrum/Chromium
    In order to run our JS-enabled tests in CI using the
    Cuprite gem we
    
    - add the chromium driver to the test build
    - pass the `no-sandbox` option for Docker use as described
      in Cuprite and Ferrum

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,13 +7,17 @@ Capybara.default_max_wait_time = 5
 Capybara.disable_animation = true
 Capybara.javascript_driver = :cuprite
 Capybara.register_driver(:cuprite) do |app|
+  browser_options = {}.tap do |opts|
+    opts["no-sandbox"] = nil if ENV["CI"]
+  end
   Capybara::Cuprite::Driver.new(
     app,
     window_size: [1200, 800],
     js_errors: true,
     timeout: 10,
     process_timeout: 15,
-    inspector: ENV["INSPECTOR"]
+    inspector: ENV["INSPECTOR"],
+    browser_options: browser_options
   )
 end
 Capybara.asset_host = "http://localhost:3000"
