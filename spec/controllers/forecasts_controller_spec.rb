2e8b66ff19eb9d0c49612090b958723819df635d
Ed Davey <ed@dxw.com>
2024-10-24 14:32:21 +0100

    Refactor spec to define fixture with Ruby rather than JSON
    This feels like cruft from an earlier naive implementation
    which handled JSON?

--- spec/controllers/forecasts_controller_spec.rb
+++ spec/controllers/forecasts_controller_spec.rb
@@ -8,45 +8,33 @@ RSpec.describe ForecastsController do
   end
 
   describe "GET :show" do
-    let(:json) do
-      <<~JSON
-        {
-          "forecastdate": "01-10-2024 14:40",
-          "timestamp": 1727793654317.342,
-          "zones": [
-            {
-              "forecasts": [
-                {
-                  "NO2": 1,
-                  "O3": 2,
-                  "PM10": 1,
-                  "PM2.5": 1,
-                  "forecast_date": "2024-10-01",
-                  "non_pollution_version": null,
-                  "pollen": -999,
-                  "pollution_version": 202410011407,
-                  "rain_am": 1.31,
-                  "rain_pm": 3.01,
-                  "temp_max": 14.0,
-                  "temp_min": 10.4,
-                  "total": 2,
-                  "total_status": "LOW",
-                  "uv": 1,
-                  "wind_am": 5.3,
-                  "wind_pm": 6.0
-                }
-              ],
-              "zone_id": 14,
-              "zone_name": "Haringey",
-              "zone_type": 1
-            }
-          ]
-        }
-      JSON
-    end
-
     let(:forecasts) do
-      ForecastFactory.build(JSON.parse(json))
+      ForecastFactory.build(
+        {"forecastdate" => "01-10-2024 14:40",
+         "timestamp" => 1727793654317.342,
+         "zones" =>
+          [{"forecasts" =>
+            [{"NO2" => 1,
+              "O3" => 2,
+              "PM10" => 1,
+              "PM2.5" => 1,
+              "forecast_date" => "2024-10-01",
+              "non_pollution_version" => nil,
+              "pollen" => -999,
+              "pollution_version" => 202410011407,
+              "rain_am" => 1.31,
+              "rain_pm" => 3.01,
+              "temp_max" => 14.0,
+              "temp_min" => 10.4,
+              "total" => 2,
+              "total_status" => "LOW",
+              "uv" => 1,
+              "wind_am" => 5.3,
+              "wind_pm" => 6.0}],
+            "zone_id" => 14,
+            "zone_name" => "Haringey",
+            "zone_type" => 1}]}
+      )
     end
 
     it "obtains forecasts for the default zone (Southwark) from the CercApiClient" do
