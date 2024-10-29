8dca429e5bf2094e7152378827beb167e19979c0
Ed Davey <ed@dxw.com>
2024-10-29 18:27:02 +0000

    Load deserialised forecasts from CachedForecast#data

--- spec/controllers/forecasts_controller_spec.rb
+++ spec/controllers/forecasts_controller_spec.rb
@@ -9,36 +9,10 @@ RSpec.describe ForecastsController do
 
   describe "GET :show" do
     let(:forecasts) do
-      ForecastFactory.build(
-        cerc_forecasts:
-          {"forecastdate" => "01-10-2024 14:40",
-           "timestamp" => 1727793654317.342,
-           "zones" =>
-            [{"forecasts" =>
-              [{"NO2" => 1,
-                "O3" => 2,
-                "PM10" => 1,
-                "PM2.5" => 1,
-                "forecast_date" => "2024-10-01",
-                "non_pollution_version" => nil,
-                "pollen" => -999,
-                "pollution_version" => 202410011407,
-                "rain_am" => 1.31,
-                "rain_pm" => 3.01,
-                "temp_max" => 14.0,
-                "temp_min" => 10.4,
-                "total" => 2,
-                "total_status" => "LOW",
-                "uv" => 1,
-                "wind_am" => 5.3,
-                "wind_pm" => 6.0}],
-              "zone_id" => 14,
-              "zone_name" => "Haringey",
-              "zone_type" => 1}]}
-      )
+      FactoryBot.build(:cached_forecast)
     end
 
-    context "when no zone is given" do
+    context "when NO zone is given" do
       it "obtains forecasts for the default zone (Southwark) from the CercForecastService" do
         allow(CercForecastService).to receive(:latest_forecasts_for).and_return(forecasts)
 
@@ -68,18 +42,20 @@ RSpec.describe ForecastsController do
 
     it "asks the forecasts for any alerts and assigns to instance variable" do
       air_quality_alert = double("air quality alert")
-      forecast_1 = instance_double(Forecast, alerts: [])
-      forecast_2 = instance_double(Forecast, alerts: [air_quality_alert])
+      forecast_1 = FactoryBot.build(:forecast)
+      forecast_2 = FactoryBot.build(:forecast)
 
-      allow(CercForecastService).to receive(:latest_forecasts_for).and_return([
-        forecast_1,
-        forecast_2
-      ])
+      allow(forecast_1).to receive(:alerts).and_return([])
+      allow(forecast_2).to receive(:alerts).and_return([air_quality_alert])
+
+      cached_forecast = FactoryBot.build(:cached_forecast).tap do |cf|
+        allow(cf).to receive(:data).and_return([forecast_1, forecast_2])
+      end
+
+      allow(CercForecastService).to receive(:latest_forecasts_for)
+        .and_return(cached_forecast)
 
       get :show
-
-      expect(forecast_1).to have_received(:alerts)
-      expect(forecast_2).to have_received(:alerts)
 
       expect(assigns(:air_quality_alerts)).to eq([air_quality_alert])
     end
