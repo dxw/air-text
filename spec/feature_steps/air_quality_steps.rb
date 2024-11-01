2d0c48ab3c0a8a0b55edc18f5081375eee311675
Patrick Fleming <patrick.fleming@dxw.com>
2024-11-01 09:15:28 +0000

    Add integration tests for air quality alerts

--- spec/feature_steps/air_quality_steps.rb
+++ spec/feature_steps/air_quality_steps.rb
@@ -28,7 +28,25 @@ module AirQualitySteps
     click_link("View forecasts")
   end
 
+  def when_i_look_at_the_forecasts_v2
+    visit root_path
+    click_link("View new style forecasts")
+  end
+
   def expect_to_see_alert_date_for(day)
+    date = case day
+    when :today
+      Date.today.strftime("%d %b %Y")
+    when :tomorrow
+      Date.tomorrow.strftime("%d %b %Y")
+    when :day_after_tomorrow
+      (Date.tomorrow + 1.day).strftime("%d %b %Y")
+    end
+
+    expect(page).to have_content("air quality alert for #{date}")
+  end
+
+  def expect_to_see_alert_date_for_v2(day)
     date = case day
     when :today
       Date.today.strftime("%d %b %Y")
@@ -54,12 +72,23 @@ module AirQualitySteps
     expect(page).to have_css(".alert-level", text: label)
   end
 
+  def expect_to_see_alert_level_v2(level)
+    label = case level
+    when :moderate
+      "Moderate"
+    when :high
+      "High"
+    when :very_high
+      "Very high"
+    end
+
+    expect(page).to have_css(".daqi-label", text: label)
+  end
+
   def expect_to_see_guidance_for(level)
     expect(page).to have_content(I18n.t("air_quality_alert.#{level}.guidance.title"))
     expect(page).to have_content(
-      ActionController::Base.helpers.strip_tags(
-        I18n.t("air_quality_alert.#{level}.guidance.detail_html")
-      )
+      I18n.t("air_quality_alert.#{level}.guidance.detail_html").truncate(20, omission: "")
     )
   end
 
@@ -68,6 +97,33 @@ module AirQualitySteps
       expect_to_see_alert_date_for(:today)
       expect_to_see_alert_level(:high)
       expect_to_see_guidance_for(:high)
+    end
+  end
+
+  def then_i_see_an_air_quality_alert_of_high_for_today_v2
+    within(".today[data-date='#{Date.today}']") do
+      expect_to_see_alert_level_v2(:high)
+    end
+    within(".alert-guidance") do
+      expect_to_see_guidance_for(:high)
+    end
+  end
+
+  def then_i_see_an_air_quality_alert_of_moderate_for_tomorrow_v2
+    within(".tomorrow[data-date='#{Date.tomorrow}']") do
+      expect_to_see_alert_level_v2(:moderate)
+    end
+    within(".alert-guidance") do
+      expect_to_see_guidance_for(:moderate)
+    end
+  end
+
+  def then_i_see_an_air_quality_alert_of_v_high_for_day_after_tomorrow_v2
+    within(".day_after_tomorrow[data-date='#{Date.tomorrow + 1.day}']") do
+      expect_to_see_alert_level_v2(:very_high)
+    end
+    within(".alert-guidance") do
+      expect_to_see_guidance_for(:very_high)
     end
   end
 
