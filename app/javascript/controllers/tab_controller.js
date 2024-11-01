8109bce22d9f6cb02dfd7ec9feac41528a03716c
Patrick Fleming <patrick.fleming@dxw.com>
2024-11-01 09:20:47 +0000

    Update JS active/inactive class toggle
    We now need to toggle an inactive class as well as an active class, because of the interaction of the active class with the daqi-level classes. In cases of alerts, we want to override the active class with the relevant daqi-level class, but where that tab is not active, we want to override that again with our inactive styling.

--- app/javascript/controllers/tab_controller.js
+++ app/javascript/controllers/tab_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="tab"
 export default class extends Controller {
-  static classes = ["active"];
+  static classes = ["active", "inactive"];
   static targets = ["day"];
 
   connect() {}
@@ -10,11 +10,13 @@ export default class extends Controller {
   switch_tab() {
     this.makeAllTabsInactive();
     this.dayTarget.classList.toggle(this.activeClass);
+    this.dayTarget.classList.toggle(this.inactiveClass);
   }
 
   makeAllTabsInactive() {
-    document
-      .querySelectorAll(".tabs .tab")
-      .forEach((el) => el.classList.remove(this.activeClass));
+    document.querySelectorAll(".tabs .tab").forEach((el) => {
+      el.classList.remove(this.activeClass);
+      el.classList.add(this.inactiveClass);
+    });
   }
 }
--- spec/feature_steps/forecast_steps.rb
+++ spec/feature_steps/forecast_steps.rb
@@ -114,6 +114,9 @@ module ForecastSteps
   def then_i_see_that_the_tomorrow_tab_is_active
     expect(page).to have_css(".tab.tomorrow.active")
 
+    expect(page).to have_css(".tab.today.inactive")
+    expect(page).to have_css(".tab.day_after_tomorrow.inactive")
+
     expect(page).not_to have_css(".tab.today.active")
     expect(page).not_to have_css(".tab.day_after_tomorrow.active")
   end
@@ -121,12 +124,18 @@ module ForecastSteps
   def and_i_see_that_the_today_tab_is_active
     expect(page).to have_css(".tab.today.active")
 
+    expect(page).to have_css(".tab.tomorrow.inactive")
+    expect(page).to have_css(".tab.day_after_tomorrow.inactive")
+
     expect(page).not_to have_css(".tab.tomorrow.active")
     expect(page).not_to have_css(".tab.day_after_tomorrow.active")
   end
 
   def then_i_see_that_the_day_after_tomorrow_tab_is_active
     expect(page).to have_css(".tab.day_after_tomorrow.active")
+
+    expect(page).to have_css(".tab.today.inactive")
+    expect(page).to have_css(".tab.tomorrow.inactive")
 
     expect(page).not_to have_css(".tab.today.active")
     expect(page).not_to have_css(".tab.tomorrow.active")
