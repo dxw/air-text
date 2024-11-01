39c6cb6aaf643091ea66ee11288605518f6b480f
Patrick Fleming <patrick.fleming@dxw.com>
2024-11-01 16:09:36 +0000

    Remove v1 files
    These files are all associated with the initial, unstyled version of our service, and can be removed.

--- spec/controllers/map_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe MapController do
-  describe "GET :show" do
-    it "renders the _show_ template" do
-      get :show
-
-      expect(response).to render_template("show")
-    end
-  end
-end
