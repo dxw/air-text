e060cec133b0fad5523c5238d3a714e31a22426b
Patrick Fleming <patrick.fleming@dxw.com>
2024-11-05 15:59:44 +0000

    Update alert stylings for air pollution tabs
    If today has an alert, we always want to display the alert stylings. For tomorrow and the day after tomorrow, if they have alerts, we only want to display the alert stylings when the tab is selected.
    
    I have opted for a slightly verbose Javascript implementation that extends the Stimulus code that was already in place, rather than bringing the tabs into the Turbo stream that is already in place for this action. However, if any part of this design becomes any more complex, this would probably be a good candidate to refactor along those lines.

--- app/javascript/controllers/tab_controller.js
+++ app/javascript/controllers/tab_controller.js
@@ -3,14 +3,17 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="tab"
 export default class extends Controller {
   static classes = ["active", "inactive"];
-  static targets = ["day"];
+  static targets = ["dayPrediction", "day", "daqiValue"];
 
   connect() {}
 
   switch_tab() {
     this.makeAllTabsInactive();
-    this.dayTarget.classList.toggle(this.activeClass);
-    this.dayTarget.classList.toggle(this.inactiveClass);
+    this.removeDaqiAlertClasses();
+
+    this.addAlertClassIfNotTodayAndHasAirQualityAlert();
+    this.dayPredictionTarget.classList.toggle(this.activeClass);
+    this.dayPredictionTarget.classList.toggle(this.inactiveClass);
   }
 
   makeAllTabsInactive() {
@@ -18,5 +21,39 @@ export default class extends Controller {
       el.classList.remove(this.activeClass);
       el.classList.add(this.inactiveClass);
     });
+  }
+
+  addAlertClassIfNotTodayAndHasAirQualityAlert() {
+    const daqiValue = this.getDaqiValue();
+    if (this.dayTarget.innerText !== "Today" && daqiValue > 3) {
+      this.addDaqiAlertClass();
+    }
+  }
+
+  addDaqiAlertClass() {
+    const daqiValue = this.getDaqiValue();
+    this.dayPredictionTarget.classList.add(
+      `daqi-alert-after-today-selected-level-${daqiValue}`
+    );
+  }
+
+  removeDaqiAlertClasses() {
+    document.querySelectorAll(".tabs .tab").forEach((el) => {
+      const daqiAlertClassRegex = new RegExp(
+        "daqi-alert-after-today-selected-level-(\\d{1,2})"
+      );
+
+      const fullClassName = Array.from(el.classList).find((className) =>
+        className.match(daqiAlertClassRegex)
+      );
+
+      if (fullClassName) {
+        el.classList.remove(fullClassName);
+      }
+    });
+  }
+
+  getDaqiValue() {
+    return this.daqiValueTarget.innerText.split("/")[0].split(" ")[1];
   }
 }
