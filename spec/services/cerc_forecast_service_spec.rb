322c0fe11d00c9a24bf4af191d3fc5dc4e8f9c1b
joseph@dxw.com <joseph@dxw.com>
2024-11-14 11:45:21 +0000

    Refactor CercForecastServiceSpec
    As a general rule we should test behaviour not implementation. That means we should be testing what the cached forecast is, not which methods were called along the way.
    
    The exception to this is the call to the API because we have stubbed the API.

--- spec/services/cerc_forecast_service_spec.rb
+++ spec/services/cerc_forecast_service_spec.rb
@@ -1,53 +1,13 @@
 RSpec.describe CercForecastService do
   describe "::latest_forecasts_for(zone)" do
-    let(:zone) { FactoryBot.build(:zone, name: "Barnet") }
-    let(:latest_forecasts_from_api) { double("latest_forecasts_from_api") }
-    let(:latest_forecast_from_cache) { double("latest_forecast_from_cache") }
-
-    before do
-      allow(CachedForecast).to receive(:stale?).and_return(true)
-      allow(CachedForecast).to receive(:latest_for).and_return(latest_forecast_from_cache)
-      allow(CachedForecast).to receive(:store).and_return(true)
-      allow(CercApiClient).to receive(:latest_forecasts).and_return(latest_forecasts_from_api)
-    end
+    let(:zone) { FactoryBot.create(:zone, cerc_id: 29) }
 
     context "when the cache is stale" do
-      let(:latest_forecasts_from_api) {
-        {
-          "forecastdate" => "02-10-2024 15:16",
-          "timestamp" => 1727882190223.2139,
-          "zones" => [
-            {
-              "forecasts" => [{}, {}, {}],
-              "zone_id" => 1,
-              "zone_name" => "Barking and Dagenham",
-              "zone_type" => 1
-            },
-            {
-              "forecasts" => [{}, {}, {}],
-              "zone_id" => 2,
-              "zone_name" => "Barnet",
-              "zone_type" => 1
-            }
-          ]
-        }
-      }
-
-      let(:built_forecasts_for_barking) { double("built_forecasts_for_barking") }
-      let(:built_forecasts_for_barnet) { double("built_forecasts_for_barnet") }
+      let(:latest_forecasts_from_api) { Fixtures::API.all_forecasts }
 
       before do
         allow(CachedForecast).to receive(:stale?).and_return(true)
-
-        allow(ForecastFactory).to receive(:build).with(
-          cerc_forecasts: latest_forecasts_from_api,
-          zone_id: 1
-        ).and_return(built_forecasts_for_barking)
-
-        allow(ForecastFactory).to receive(:build).with(
-          cerc_forecasts: latest_forecasts_from_api,
-          zone_id: 2
-        ).and_return(built_forecasts_for_barnet)
+        allow(CercApiClient).to receive(:latest_forecasts).and_return(latest_forecasts_from_api)
       end
 
       it "asks the CercApiClient for the latest_forecasts (for all zones)" do
@@ -56,44 +16,25 @@ RSpec.describe CercForecastService do
         expect(CercApiClient).to have_received(:latest_forecasts).with(no_args)
       end
 
-      it "builds forecasts for each zone" do
-        CercForecastService.latest_forecasts_for(zone)
-
-        expect(ForecastFactory).to have_received(:build).with(
-          cerc_forecasts: latest_forecasts_from_api,
-          zone_id: 2
-        )
-        expect(ForecastFactory).to have_received(:build).with(
-          cerc_forecasts: latest_forecasts_from_api,
-          zone_id: 1
-        )
-      end
-
       it "caches a built forecast for each zone" do
-        CercForecastService.latest_forecasts_for(zone)
-
-        expect(CachedForecast).to have_received(:store).with(built_forecasts_for_barking)
-        expect(CachedForecast).to have_received(:store).with(built_forecasts_for_barnet)
-      end
-
-      it "returns the new forecast for the given zone" do
-        CercForecastService.latest_forecasts_for(zone)
-
-        expect(CachedForecast).to have_received(:latest_for).with(zone)
+        api_temperature = latest_forecasts_from_api["zones"].first["forecasts"].first["temp_max"]
+        cached_temperature = CercForecastService.latest_forecasts_for(zone).data.first.temperature.max
+        expect(cached_temperature).to eq(api_temperature)
       end
     end
 
     context "when the cache is NOT stale" do
-      let(:latest_forecast) { double("latest_forecast") }
-
+      let(:latest_forecast_from_cache) { double("CachedForecast") }
       before do
         allow(CachedForecast).to receive(:stale?).and_return(false)
+        allow(CercApiClient).to receive(:latest_forecasts)
+        allow(CachedForecast).to receive(:latest_for).and_return(latest_forecast_from_cache)
       end
 
-      it "retrieves the cached forecast for the given zone" do
+      it "does not ask the CercApiClient for the latest_forecasts" do
         CercForecastService.latest_forecasts_for(zone)
 
-        expect(CachedForecast).to have_received(:latest_for).with(zone)
+        expect(CercApiClient).not_to have_received(:latest_forecasts)
       end
 
       it "returns the cached forecast for the given zone" do
