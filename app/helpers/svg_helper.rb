5df9c6be3e402467ac1d6936c811117a60f274cd
joseph@dxw.com <joseph@dxw.com>
2024-11-26 12:37:49 +0000

    Add SVG renderer
    Currently we are rendering SVGs by changing their extension to `.html.erb`. This is confusing and means they don't get included in the asset pipeline.
    
    However, if we change them to be `.svg` files, the normal `render` method cannot help us. Rails doesn't have an easy mechanism for rendering SVGs, so I have added a helper for this.

---
+++ app/helpers/svg_helper.rb
@@ -0,0 +1,20 @@
+module SvgHelper
+  def render_svg(svg_path, classes: nil, tag: nil)
+    asset = find_asset(svg_path)
+    return unless asset
+    return asset.html_safe if tag.nil?
+
+    content_tag(tag, asset.html_safe, class: classes)
+  end
+
+  def find_asset(path)
+    return unless path.present?
+
+    if Rails.env.production? || Rails.env.staging?
+      asset_path = Rails.application.assets_manifest.assets[path]
+      File.read(Rails.root.join("public", "assets", asset_path))
+    else
+      Rails.application.assets[path]&.source
+    end
+  end
+end
