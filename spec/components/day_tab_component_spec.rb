cc1cd498f078059ebbd484e03c64ecf4c2de4e84
joseph@dxw.com <joseph@dxw.com>
2024-11-27 14:26:28 +0000

    Fix warning icon having the wrong colour when pollution is very high

--- spec/components/day_tab_component_spec.rb
+++ spec/components/day_tab_component_spec.rb
@@ -107,20 +107,28 @@ RSpec.describe DayTabComponent, type: :component do
 
   describe "icon_stroke_colour_class" do
     let(:component) {
-      forecast = FactoryBot.build(:forecast, air_pollution: FactoryBot.build(:air_pollution_prediction, label: label))
+      forecast = FactoryBot.build(:forecast, air_pollution: FactoryBot.build(:air_pollution_prediction, level))
       DayTabComponent.new(forecast: forecast, day: :today)
     }
 
-    context "when the air pollution label is _HIGH_" do
-      let(:label) { "HIGH" }
+    context "when the air pollution label is _Very high_" do
+      let(:level) { :very_high }
 
       it "returns _stroke-white_" do
         expect(component.icon_stroke_colour_class).to eq("stroke-white")
       end
     end
 
-    context "when the air pollution label is _MODERATE_" do
-      let(:label) { "MODERATE" }
+    context "when the air pollution label is _High_" do
+      let(:level) { :high }
+
+      it "returns _stroke-white_" do
+        expect(component.icon_stroke_colour_class).to eq("stroke-white")
+      end
+    end
+
+    context "when the air pollution label is _Moderate_" do
+      let(:level) { :moderate }
 
       it "returns _stroke-black_" do
         expect(component.icon_stroke_colour_class).to eq("stroke-black")
