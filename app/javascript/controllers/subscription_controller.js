be8e5dec51b7660f0b35aa1d51a06d47da16ae6f
joseph@dxw.com <joseph@dxw.com>
2025-01-09 17:00:37 +0000

    Update URL on each form page
    Because we're using Turbo, this doesn't happen by default.

--- app/javascript/controllers/subscription_controller.js
+++ app/javascript/controllers/subscription_controller.js
@@ -18,6 +18,16 @@ export default class SubscriptionController extends Controller {
 
   connect() {
     useDebounce(this);
+
+    // Update the URL to current step
+    ["turbo:load", "turbo:frame-load", "turbo:render"].forEach((eventName) => {
+      document.addEventListener(eventName, () => {
+        const currentStep = document.getElementById(
+          "subscription_form_current_step"
+        ).value;
+        this.updateUrl(currentStep);
+      });
+    });
   }
 
   // Contact details
@@ -234,5 +244,9 @@ export default class SubscriptionController extends Controller {
     if (event.keyCode === 37) {
       input.previousElementSibling.focus();
     }
+  }
+
+  updateUrl(url) {
+    window.history.pushState({}, "", url);
   }
 }
