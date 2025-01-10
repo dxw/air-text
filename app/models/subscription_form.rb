4a90550c889018b6dab2cd964bd83ecb965bf598
joseph@dxw.com <joseph@dxw.com>
2025-01-10 17:57:36 +0000

    Add progress bar to subscription form

--- app/models/subscription_form.rb
+++ app/models/subscription_form.rb
@@ -121,8 +121,8 @@ class SubscriptionForm
   end
 
   def after_step?(step_name)
-    current_step_index = current_step.present? ? SubscriptionsController::ALL_STEPS.index(current_step.to_sym) : 0
-    this_step_index = SubscriptionsController::ALL_STEPS.index(step_name.to_sym) || 0
+    current_step_index = current_step.present? ? SubscriptionsController::ALL_STEPS.flatten.index(current_step.to_sym) : 0
+    this_step_index = SubscriptionsController::ALL_STEPS.flatten.index(step_name.to_sym) || 0
 
     current_step_index >= this_step_index
   end
