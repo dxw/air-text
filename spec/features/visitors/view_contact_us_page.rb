825dabb6dc8a543d912f2689ee42a955b97db472
joseph@dxw.com <joseph@dxw.com>
2025-03-21 13:20:17 +0000

    Add contact us page

---
+++ spec/features/visitors/view_contact_us_page.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.feature "Contact us page" do
+  before do
+    forecasts = [
+      Fixtures::API.zone_forecast(day: :today),
+      Fixtures::API.zone_forecast(day: :tomorrow),
+      Fixtures::API.zone_forecast(day: :day_after_tomorrow)
+    ]
+    HttpStubs.stub_cerc_api_with(forecasts)
+  end
+
+  describe "Visit the contact us page" do
+    it "should display the contact us page" do
+      visit "contact"
+      expect(page).to have_content "Contact us"
+    end
+
+    it "is accessible", js: true do
+      visit "contact"
+      expect(page).to be_accessible
+    end
+  end
+end
